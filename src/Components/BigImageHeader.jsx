a1184438977750e700f026ccac1bf4fa36f1c644
Cosmeeeen <iliemcosmin@yahoo.ro>
2021-04-02 23:26:55 +0300

    fixed it for mobile by adding separate class, can't get it perfect otherwise

--- src/Components/BigImageHeader.jsx
+++ src/Components/BigImageHeader.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { isMobile } from 'react-device-detect';
 
 const BigImageHeader = ({ title, subtitle }) => {
 
     return (
-        <div className="bigImageHeaderContainer">
+        <div className={`bigImageHeaderContainer ${isMobile ? 'mobile' : ''}`}>
             <h1>{title}</h1>
             <h3>{subtitle}</h3>
         </div>
