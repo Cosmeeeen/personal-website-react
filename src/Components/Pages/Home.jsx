5ada84214f38bc893ea313e662ed4ec7b0b5691f
Cosmeeeen <iliemcosmin@yahoo.ro>
2021-04-02 23:19:20 +0300

    Configured big title image, needs work on mobile

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -4,8 +4,7 @@ import BigImageHeader from '../BigImageHeader';
 const Home = () => {
     return (
         <div className="homeContainer">
-            <h1>This is the home page.</h1>
-            <BigImageHeader title="Hello" subtitle="world"/>
+            <BigImageHeader title="Cosmin Ilie" subtitle="Web Developer"/>
             <p><a href="https://www.pexels.com/photo/grayscale-photo-of-volcano-997443/" target="_blank" rel="noreferrer">Grayscale Photo of Volcano by Tyler Lastovich from Pexels </a></p>
         </div>
     );
