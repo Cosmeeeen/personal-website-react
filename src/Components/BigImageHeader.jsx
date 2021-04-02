ea8196b2d6711d4ee0fa7e432aa13dbb587dfc94
Cosmeeeen <iliemcosmin@yahoo.ro>
2021-04-02 22:47:40 +0300

    Setup for big image header

--- src/Components/BigImageHeader.jsx
+++ src/Components/BigImageHeader.jsx
@@ -1,1 +1,14 @@
+import React from 'react';
 
+const BigImageHeader = ({ title, subtitle }) => {
+
+    return (
+        <div className="bigImageHeaderContainer">
+            <h1>{title}</h1>
+            <h3>{subtitle}</h3>
+        </div>
+    );
+
+};
+
+export default BigImageHeader;
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import BigImageHeader from '../BigImageHeader';
 
 const Home = () => {
     return (
         <div className="homeContainer">
             <h1>This is the home page.</h1>
-
+            <BigImageHeader title="Hello" subtitle="world"/>
             <p><a href="https://www.pexels.com/photo/grayscale-photo-of-volcano-997443/" target="_blank" rel="noreferrer">Grayscale Photo of Volcano by Tyler Lastovich from Pexels </a></p>
         </div>
     );
