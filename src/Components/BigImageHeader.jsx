4343f96332c37fb0b7e2ffa3528dcbb5276b6dea
Cosmeeeen <iliemcosmin@yahoo.ro>
2021-04-03 16:09:52 +0300

    added credit to big image and fixed mobile fonts

--- src/Components/BigImageHeader.jsx
+++ src/Components/BigImageHeader.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { isMobile } from 'react-device-detect';
 
-const BigImageHeader = ({ title, subtitle }) => {
+const BigImageHeader = ({ title, subtitle, creditText, creditLink }) => {
+
+    const imageCredit = (
+        <a href={creditLink} target="_blank" rel="noreferrer">{creditText}</a>
+    );
 
     return (
-        <div className={`bigImageHeaderContainer ${isMobile ? 'mobile' : ''}`}>
-            <h1>{title}</h1>
-            <h3>{subtitle}</h3>
+        <div>
+            <div className={`bigImageHeaderContainer ${isMobile ? 'mobile' : ''}`}>
+                <h1>{title}</h1>
+                <h3>{subtitle}</h3>
+                {creditText !== "" ? imageCredit : null}
+            </div>
         </div>
     );
 
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -4,8 +4,12 @@ import BigImageHeader from '../BigImageHeader';
 const Home = () => {
     return (
         <div className="homeContainer">
-            <BigImageHeader title="Cosmin Ilie" subtitle="Web Developer"/>
-            <p><a href="https://www.pexels.com/photo/grayscale-photo-of-volcano-997443/" target="_blank" rel="noreferrer">Grayscale Photo of Volcano by Tyler Lastovich from Pexels </a></p>
+            <BigImageHeader 
+                title="Cosmin Ilie" 
+                subtitle="Web Developer" 
+                creditText="Grayscale Photo of Volcano by Tyler Lastovich from Pexels"
+                creditLink="https://www.pexels.com/photo/grayscale-photo-of-volcano-997443/"
+            />
         </div>
     );
 };
